4092545a1626bb932ea873105871cb03d367c544
franco0310 <francocorvalan2@gmail.com>
2024-03-03 19:10:11 -0300

    Task. Create a Model with its Fields

--- motorcycle_registry/models/models.py
+++ motorcycle_registry/models/models.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class motorcycle_registry(models.Model):
-#     _name = 'motorcycle_registry.motorcycle_registry'
-#     _description = 'motorcycle_registry.motorcycle_registry'
+class MotorcycleRegistry(models.Model):
+     _name = 'motorcycle.registry'
+     _description = 'Motorcycle Registry'
+     _rec_name = 'registry_number'
 
-#     name = fields.Char()
+     certificate_title = fields.Binary(string='Certificate Title')
+     current_mileage = fields.Float(string='Current Mileage')
+     first_name = fields.Char(string='First Name', required=True)
+     last_name = fields.Char(string='Last Name', required=True)
+     license_plate = fields.Char(string='License Plate')
+     registry_date = fields.Date(string='Registry Date')
+     registry_number = fields.Char(string='Registry Number', required=True)
+     vin = fields.Char(string='VIN', required=True)
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
